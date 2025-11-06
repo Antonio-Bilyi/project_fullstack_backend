77798e9f24359e3c30692348c62d928782bb671c
Anton <anthonybelyj@protonmail.com>
2025-11-06 19:51:50 +0200

    still setup Server

---
+++ src/middlewares/isValidId.js
@@ -0,0 +1,12 @@
+import { isValidObjectId } from "mongoose";
+import createHttpError from "http-errors";
+
+export default function isValidId(req, res, next) {
+    const { storyId } = req.params;
+
+    if (!isValidObjectId(storyId)) {
+        throw createHttpError(400, "Bad Request");
+    }
+
+    next();
+};
---
+++ src/utils/uploadToCloudinary.js
@@ -0,0 +1,13 @@
+import cloudinary from 'cloudinary';
+import getEnvVar from './getEnvVars';
+
+
+cloudinary.v2.config({
+    cloud_name: getEnvVar("CLOUDINARY_CLOUD_NAME"),
+    api_key: getEnvVar("CLOUDINARY_API_KEY"),
+    api_secret: getEnvVar("CLOUDINARY_SECRET"),
+});
+
+export default function uploadToCloudinary(filePath) {
+    return cloudinary.v2.uploader.upload(filePath);
+};
