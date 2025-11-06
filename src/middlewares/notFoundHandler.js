f3fdad6fccc26dbe8cd9879f7aa5ff5ea300088d
Anton Bilyi <anthonybelyj@protonmail.com>
2025-11-06 12:12:15 +0200

    setup Server

---
+++ src/middlewares/notFoundHandler.js
@@ -0,0 +1,19 @@
+import { HttpError } from 'http-errors';
+
+export default function notFoundHandler(err, req, res, next) {
+    if (err instanceof HttpError) {
+        res.status(err.status).json({
+            status: err.status,
+            message: err.name,
+            data: err,
+        });
+
+        return;
+    };
+
+    res.status(404).json({
+        status: 404,
+        message: "Route not found",
+        data: err.message,
+    });
+};
---
+++ src/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino';
+import pinoHttp from 'pino-http';
+import getEnvVar from './utils/getEnvVars.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+
+const PORT = Number(getEnvVar("PORT") || 8080);
+
+export default function setupServer() {
+    const app = express();
+
+    app.use(express.json());
+
+    app.use(cors());
+
+    const logger = pino({
+        transport: {
+            target: "pino-pretty",
+            options: { colorize: true },
+        }
+    });
+
+    app.use(pinoHttp({ logger }));
+
+    app.use(notFoundHandler);
+
+    app.use(errorHandler);
+
+    app.listen(PORT, (error) => {
+        if (error) {
+            throw error;
+        }
+
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
