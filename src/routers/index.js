70a626b06f72f90ecf8c90aab3a5b99185915b38
Anton <anthonybelyj@protonmail.com>
2025-11-07 09:21:32 +0200

    fixed server

---
+++ src/routers/index.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import authRouter from './auth.js';
+import storiesRouter from './stories.js';
+import usersRouter from './users.js';
+
+const router = Router();
+
+
+export default router;
