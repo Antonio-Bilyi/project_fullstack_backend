55aed5bbcc8153d057e72f1c417b48ef1e97b882
Anton <anthonybelyj@protonmail.com>
2025-11-07 16:06:10 +0200

    fixed validateBody

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -5,7 +5,6 @@ import { loginUserController } from '../controllers/auth/loginUserCntrl.js';
 import validateBody from '../middlewares/validateBody.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth/registerUserCntrl.js';
-import validateBody from '../middlewares/validateBody.js';
 import { logoutUserController } from '../controllers/auth/logoutUserCntrl.js';
 
 const router = Router();
@@ -21,6 +20,7 @@ router.post(
   ctrlWrapper(loginUserController),
 );
 
+router.post(
   '/logout',
   ctrlWrapper(logoutUserController));
   
