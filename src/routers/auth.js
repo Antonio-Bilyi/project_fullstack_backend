d3da4fee90e591eaa629b7c532b6bca3e9b7f329
ShumyloYana <zolotushko1@gmail.com>
2025-11-07 13:58:36 +0200

    loginUser

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
-
+import ctrlWrapper  from '../utils/ctrlWrapper.js';
+import { loginSchema} from '../validation/auth.js';
+import { loginUserController } from '../controllers/auth/loginUserCntrl.js';
+import validateBody from '../middlewares/validateBody.js';
 
 const router = Router();
 
-
+router.post(
+  '/login',
+  validateBody(loginSchema),
+  ctrlWrapper(loginUserController),
+);
 
 export default router;
