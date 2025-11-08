e6f9afd4273f9b8b94800222804c0cdf7a7ecee4
Andrii <simonandrij717@gmail.com>
2025-11-08 19:44:46 +0100

    fix route patch and add middleware authenticate

--- src/routers/stories.js
+++ src/routers/stories.js
@@ -1,9 +1,25 @@
 import { Router } from 'express';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import { getAllStoriesCntrl } from '../controllers/stories/getAllStoriesCntrl.js';
+import isValidId from '../middlewares/isValidId.js';
+import validateBody from '../middlewares/validateBody.js';
+import { updateStorySchema } from '../validation/updateStories.js';
+import { patchStoriesController } from '../controllers/stories/updateStoryCntrl.js';
+import { upload } from '../middlewares/upload.js';
+import { authenticate } from '../middlewares/auth.js';
 
 const router = Router();
 
+router.use(authenticate);
+
 router.get('/', ctrlWrapper(getAllStoriesCntrl));
+
+router.patch(
+  '/:storyId',
+  isValidId,
+  upload.single('photo'),
+  validateBody(updateStorySchema),
+  ctrlWrapper(patchStoriesController),
+);
 
 export default router;
