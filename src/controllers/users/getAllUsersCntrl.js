5284cf5e87c56241215baf3291584860fec8706f
Anton <anthonybelyj@protonmail.com>
2025-11-08 22:38:05 +0200

    fixed getAllUsers

--- src/controllers/users/getAllUsersCntrl.js
+++ src/controllers/users/getAllUsersCntrl.js
@@ -1,16 +1,12 @@
 import { getAllUsers } from '../../services/users/getAllUsers.js';
 import parsePaginationParams from '../../utils/parsePaginationParams.js';
-import parseSortParams from '../../utils/parseSortParams.js';
 
 export const getAllUsersCntrl = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams(req.query);
 
   const users = await getAllUsers({
     page,
     perPage,
-    sortBy,
-    sortOrder,
   });
 
   res.status(200).json({
--- src/services/users/getAllUsers.js
+++ src/services/users/getAllUsers.js
@@ -1,12 +1,9 @@
 import { UsersCollection } from '../../db/models/users.js';
 import calculatePaginationData from '../../utils/calculatePaginationData.js';
-import { SORT_ORDER } from '../../constants/index.js';
 
 export const getAllUsers = async ({
   page = 1,
   perPage = 10,
-  sortOrder = SORT_ORDER.ASC,
-  sortBy = '_id',
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
@@ -17,7 +14,6 @@ export const getAllUsers = async ({
   const users = await usersQuery
     .skip(skip)
     .limit(limit)
-    .sort({ [sortBy]: sortOrder })
     .exec();
 
   const paginationData = calculatePaginationData(userCount, perPage, page);
