84842ae450c846d6ea92e13b11ccc015f72fa5d4
Remchik <art6000.7m@gmail.com>
2025-11-08 13:02:15 +0200

    Update createStoryController

--- src/controllers/stories/createStoryCntrl.js
+++ src/controllers/stories/createStoryCntrl.js
@@ -15,17 +15,20 @@ export async function createStoryController(req, res) {
   const story = await createStory({
     ...req.body,
     img,
+    ownerId: req.user.id,
     date: new Date(),
-    userId: req.user.id,
   });
 
-  await UsersCollection.findByIdAndUpdate(req.user.id, {
+  const cleanStory = story.toObject();
+  delete cleanStory.favoriteCount;
+
+  await UsersCollection.findByIdAndUpdate('6881563901add19ee16fcff5', {
     $inc: { articleAmount: 1 },
   });
 
   res.status(201).json({
     status: 201,
     message: 'Successfully created a story',
-    data: story,
+    data: cleanStory,
   });
 }
--- src/validation/story.js
+++ src/validation/story.js
@@ -1,8 +1,7 @@
 import Joi from 'joi';
 
 export const createStorySchema = Joi.object({
-  img: Joi.any().meta({ swaggerType: 'file' }).required(),
   title: Joi.string().max(80).required(),
-  description: Joi.string().max(2500).required(),
+  article: Joi.string().max(2500).required(),
   category: Joi.string().hex().length(24).required(),
 });
