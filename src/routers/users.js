153005ffe018de5ae30a75427ce0a199ca4f6f7c
Yevhen Shymka <schimka3@gmail.com>
2025-11-08 17:19:48 +0100

    update_user_doc_swager

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,14 +1,23 @@
 import { Router } from 'express';
 import { getCurrentUserCntrl } from '../controllers/users/getCurrentUserCntrl.js';
+import { updateUserProfileCntrl } from '../controllers/users/updateUserProfileCntrl.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { authenticate } from '../middlewares/auth.js';
+import validateBody from '../middlewares/validateBody.js';
+import { updateUserProfileSchema } from '../validation/users.js';
 
 const usersRouter = Router();
 
-// TODO: Add authenticate middleware when ready
-// import { authenticate } from '../middlewares/authenticate.js';
-// usersRouter.use(authenticate);
+usersRouter.use(authenticate);
 
 // GET /api/users/current - retrieve current authenticated user information
 usersRouter.get('/current', ctrlWrapper(getCurrentUserCntrl));
+
+// PATCH /api/users/profile - update current authenticated user profile
+usersRouter.patch(
+  '/profile',
+  validateBody(updateUserProfileSchema),
+  ctrlWrapper(updateUserProfileCntrl),
+);
 
 export default usersRouter;
