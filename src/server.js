d9098a3db297cdd327342c6b204571d41abe854f
Anton <anthonybelyj@protonmail.com>
2025-11-11 17:09:38 +0200

    fixed cookies

--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ export default function setupServer() {
 
   app.use(cookieParser());
   
-  app.use(cors());
+  // app.use(cors());
+
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }));
 
   const logger = pino({
     transport: {
--- src/services/auth/setupSession.js
+++ src/services/auth/setupSession.js
@@ -1,19 +1,41 @@
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../../constants/index.js';
 
+// const setupSession = (res, session) => {
+//     res.cookie("accessToken", session.accessToken, {
+//         httpOnly: true,
+//         expires: new Date(Date.now() + FIFTEEN_MINUTES),
+//     });
+
+//     res.cookie("refreshToken", session.refreshToken, {
+//         httpOnly: true,
+//         expires: new Date(Date.now() + THIRTY_DAYS),
+//     });
+//     res.cookie("sessionId", session._id, {
+//         httpOnly: true,
+//         expires: new Date(Date.now() + THIRTY_DAYS),
+//     });
+// };
+
 const setupSession = (res, session) => {
-    res.cookie("accessToken", session.accessToken, {
+    const common = {
         httpOnly: true,
+        sameSite: 'lax',
+        secure: false,
+    };
+    
+    res.cookie("accessToken", session.accessToken, {
+        ...common,
         expires: new Date(Date.now() + FIFTEEN_MINUTES),
     });
 
     res.cookie("refreshToken", session.refreshToken, {
-        httpOnly: true,
+        ...common,
         expires: new Date(Date.now() + THIRTY_DAYS),
     });
-    res.cookie("sessionId", session._id), {
-        httpOnly: true,
+    res.cookie("sessionId", session._id, {
+        ...common,
         expires: new Date(Date.now() + THIRTY_DAYS),
-    };
+    });
 };
 
 export default setupSession;
