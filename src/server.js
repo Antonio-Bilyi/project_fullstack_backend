2931dd5a7c286da1add8fdbad928ff8ddfc94fee
Anton <anthonybelyj@protonmail.com>
2025-11-12 17:37:18 +0200

    fixed

--- src/server.js
+++ src/server.js
@@ -17,10 +17,6 @@ export default function setupServer() {
   app.use('/api-docs', swaggerDocs());
   app.use(express.json());
 
-  // app.use(cookieParser());
-
-  // app.use(cors());
-
   app.use(
     cors({
       origin: 'http://localhost:3000',
