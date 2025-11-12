264b6844b8ddcb3ad6aea11fcbda901fe53ddb82
Anton <anthonybelyj@protonmail.com>
2025-11-12 14:04:33 +0200

    fixed validation Story

--- src/validation/story.js
+++ src/validation/story.js
@@ -2,6 +2,21 @@ import Joi from 'joi';
 import { isValidObjectId } from 'mongoose';
 
 export const createStorySchema = Joi.object({
+  storyImage: Joi.object({
+    fieldname: Joi.string().required(),
+    originalname: Joi.string().required(),
+    encoding: Joi.string().required(),
+    mimetype: Joi.string().valid('image/jpeg', 'image/png').required().
+      messages({
+        'any.only': 'Allowed file type are: JPG, PNG',
+        'any.required': 'Image is required',
+      }),
+    size: Joi.number().max(2 * 1024 * 1024).required().
+      messages({
+        'number.max': 'File size must not exceed 2MB',
+        'any.required': 'Image is required',
+    }),
+  }).required(),
   title: Joi.string().max(80).required(),
   article: Joi.string().max(2500).required(),
   category: Joi.string().custom((value, helper) => {
@@ -9,5 +24,5 @@ export const createStorySchema = Joi.object({
       return helper.message('Category id should be a valid mongo id');
     }
     return true;
-  }),
+  }).required(),
 });
--- src/validation/updateStories.js
+++ src/validation/updateStories.js
@@ -1,11 +1,28 @@
 import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 
 export const updateStorySchema = Joi.object({
-  photo: Joi.string(),
-
-  title: Joi.string().min(3).max(80).optional(),
-
-  description: Joi.string().max(2500).optional(),
-
-  category: Joi.string().hex().required(),
+  storyImage: Joi.object({
+    fieldname: Joi.string().required(),
+    originalname: Joi.string().required(),
+    encoding: Joi.string().required(),
+    mimetype: Joi.string().valid('image/jpeg', 'image/png').required().
+      messages({
+        'any.only': 'Allowed file type are: JPG, PNG',
+        'any.required': 'Image is required',
+      }),
+    size: Joi.number().max(2 * 1024 * 1024).required().
+      messages({
+        'number.max': 'File size must not exceed 2MB',
+        'any.required': 'Image is required',
+    }),
+  }),
+  title: Joi.string().max(80),
+  article: Joi.string().max(2500),
+  category: Joi.string().custom((value, helper) => {
+    if (value && !isValidObjectId(value)) {
+      return helper.message('Category id should be a valid mongo id');
+    }
+    return true;
+  }).required(),
 });
