f05b97984738baf29e0b7d76a9cc3c9eb12b6630
Yevgen Shymka <schimka3@gmail.com>
2025-11-13 17:32:03 +0100

    /api/users/profile

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,21 +2,6 @@ import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/users.js';
 import { SessionsCollection } from '../db/models/session.js';
 
-/**
- * Middleware для аутентифікації користувача через JWT токен
- * 
- * Перевіряє наявність та валідність access token з cookies,
- * знаходить активну сесію користувача в БД та додає дані
- * користувача до req.user для використання в наступних middleware
- * 
- * @async
- * @param {Object} req - Express request object
- * @param {Object} res - Express response object
- * @param {Function} next - Express next middleware function
- * @throws {401} Unauthorized - якщо токен відсутній, сесія не знайдена або прострочена
- * @throws {404} NotFound - якщо користувач не знайдений в БД
- * @returns {void} Додає user до req.user та викликає next()
- */
 export const authenticate = async (req, res, next) => {
   if (!req.cookies.accessToken) {
     throw createHttpError.Unauthorized('Please provide access token');
