47ddf0de88eed13bd1a55904334b467ebaa33e38
Iryna_Novikova <iris_vs@ukr.net>
2025-11-18 00:53:05 +0200

    add route getStoryById

--- src/services/stories/getStoryById.js
+++ src/services/stories/getStoryById.js
@@ -1,5 +1,7 @@
 import { StoriesCollection } from '../../db/models/stories.js';
 
 export const getStoryById = async (storyId) => {
-  return StoriesCollection.findById(storyId);
+  return StoriesCollection.findById(storyId)
+    .populate('category')
+    .populate({ path: 'ownerId', select: '_id name avatarUrl' });
 };
